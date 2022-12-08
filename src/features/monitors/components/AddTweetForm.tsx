59a51fe879f3d23cea01748b6951ef615a62be59
f0reachARR <f0reach@f0reach.me>
2022-12-08 23:55:57 +0900

    Add working tweet add form

--- src/features/monitors/components/AddTweetForm.tsx
+++ src/features/monitors/components/AddTweetForm.tsx
@@ -1,21 +1,29 @@
-import { Button, Card, CardBody, CardHeader, Heading } from '@chakra-ui/react';
+import { Button, Box } from '@chakra-ui/react';
 import React from 'react';
+import { mutate } from 'swr';
 import { TweetSelector } from '../../../components/TweetSelector';
+import { addMonitorTweet } from '../api';
 
 export const AddTweetForm = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [tweetId, setTweetId] = React.useState('');
+  const handleAdd = async () => {
+    setLoading(true);
+    await addMonitorTweet(tweetId);
+    setLoading(false);
+    mutate('/api/twitter/monitor/tweet');
+  };
   return (
-    <Card marginY={3}>
-      <CardHeader>
-        <Heading as='h4' size='md'>
-          Add Tweet Monitor
-        </Heading>
-      </CardHeader>
-      <CardBody>
-        <TweetSelector />
-        <Button marginY='4' colorScheme='green'>
-          Add
-        </Button>
-      </CardBody>
-    </Card>
+    <Box>
+      <TweetSelector onChange={(tweetId) => setTweetId(tweetId)} />
+      <Button
+        marginY='4'
+        colorScheme='green'
+        onClick={handleAdd}
+        isLoading={loading}
+      >
+        Add
+      </Button>
+    </Box>
   );
 };
