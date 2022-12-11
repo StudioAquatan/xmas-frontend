17b4657e0c0f68e26e4cf411550cd072788db87c
f0reachARR <f0reach@f0reach.me>
2022-12-11 13:30:27 +0900

    Add monitor related selection

---
+++ src/features/rules/components/RuleEventSelector.tsx
@@ -0,0 +1,75 @@
+import {
+  HStack,
+  Radio,
+  RadioGroup,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from '@chakra-ui/react';
+import React from 'react';
+import { Loading } from '../../../components/Loading';
+import { HashtagList, TweetList } from '../../monitors';
+import { Rule, RuleEventType } from '../api';
+
+interface Props {
+  onChange?: (
+    partialRule: Pick<Rule, 'event' | 'eventTweets' | 'eventHashtags'>
+  ) => unknown;
+}
+
+export const RuleEventSelector = ({ onChange }: Props) => {
+  const [tweetIds, setTweetIds] = React.useState<string[]>([]);
+  const [eventType, setType] = React.useState<RuleEventType>('none');
+  const [hashtagIds, setHashtagIds] = React.useState<number[]>([]);
+
+  const handleTabChange = (index: number) => {
+    if (index === 0) setType('none');
+    else if (index === 2) setType('hashtag');
+
+    setTweetIds([]);
+    setHashtagIds([]);
+  };
+
+  React.useEffect(() => {
+    onChange?.({
+      event: eventType,
+      eventTweets: tweetIds,
+      eventHashtags: hashtagIds,
+    });
+  }, [eventType, tweetIds, hashtagIds]);
+
+  return (
+    <Tabs variant='solid-rounded' onChange={handleTabChange}>
+      <TabList>
+        <Tab textTransform='uppercase'>None</Tab>
+        <Tab textTransform='uppercase'>Tweet</Tab>
+        <Tab textTransform='uppercase'>Hashtag</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel />
+        <TabPanel>
+          <RadioGroup
+            onChange={(type) => setType(type as RuleEventType)}
+            value={eventType}
+          >
+            <HStack spacing={3}>
+              <Radio value='fav'>Like</Radio>
+              <Radio value='retweet'>Retweet</Radio>
+              <Radio value='reply'>Reply</Radio>
+            </HStack>
+          </RadioGroup>
+          <React.Suspense fallback={<Loading />}>
+            <TweetList tweetIds={tweetIds} onSelect={setTweetIds} />
+          </React.Suspense>
+        </TabPanel>
+        <TabPanel>
+          <React.Suspense fallback={<Loading />}>
+            <HashtagList hashtagIds={hashtagIds} onSelect={setHashtagIds} />
+          </React.Suspense>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
---
+++ src/features/rules/components/RuleSumSelector.tsx
@@ -0,0 +1,44 @@
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react';
+import React from 'react';
+import { Loading } from '../../../components/Loading';
+import { TweetList, HashtagList } from '../../monitors';
+import { Rule } from '../api';
+
+interface Props {
+  onChange?: (
+    partialRule: Pick<Rule, 'collectTweets' | 'collectHashtags'>
+  ) => unknown;
+}
+
+export const RuleSumSelector = ({ onChange }: Props) => {
+  const [tweetIds, setTweetIds] = React.useState<string[]>([]);
+  const [hashtagIds, setHashtagIds] = React.useState<number[]>([]);
+
+  React.useEffect(() => {
+    onChange?.({
+      collectTweets: tweetIds,
+      collectHashtags: hashtagIds,
+    });
+  }, [tweetIds, hashtagIds]);
+
+  return (
+    <Tabs variant='enclosed'>
+      <TabList>
+        <Tab textTransform='uppercase'>Tweet</Tab>
+        <Tab textTransform='uppercase'>Hashtag</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <React.Suspense fallback={<Loading />}>
+            <TweetList tweetIds={tweetIds} onSelect={setTweetIds} />
+          </React.Suspense>
+        </TabPanel>
+        <TabPanel>
+          <React.Suspense fallback={<Loading />}>
+            <HashtagList hashtagIds={hashtagIds} onSelect={setHashtagIds} />
+          </React.Suspense>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
