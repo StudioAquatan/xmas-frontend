b855835556f2ca711406ad686db8ba6f5aecf517
f0reachARR <f0reach@f0reach.me>
2022-12-12 15:24:36 +0900

    Make text generator as function

--- src/features/rules/stores/text.ts
+++ src/features/rules/stores/text.ts
@@ -12,6 +12,14 @@ import {
   sumTypeSelectorAtom,
   sourceSelectorAtom,
   resultAtom,
+  FavRangePartialProps,
+  RetweetRangePartialProps,
+  ReplyRangePartialProps,
+  HashtagRangePartialProps,
+  SumRangePartialProps,
+  SumTypePartialProps,
+  SourcePartialProps,
+  ResultPartialProps,
 } from './atoms';
 
 export const getEventText = ({
@@ -44,76 +52,111 @@ export const eventTextSelector = selector({
   },
 });
 
+export const getRangeText = ({
+  minFav,
+  maxFav,
+  minRetweet,
+  maxRetweet,
+  minReply,
+  maxReply,
+  minHashtag,
+  maxHashtag,
+  minSum,
+  maxSum,
+  sumTarget,
+}: FavRangePartialProps &
+  RetweetRangePartialProps &
+  ReplyRangePartialProps &
+  HashtagRangePartialProps &
+  SumRangePartialProps &
+  SumTypePartialProps) => {
+  const hasRange = (min: number | null, max: number | null) => {
+    return !(min === null && max === null);
+  };
+  const range = (min: number | null, max: number | null) => {
+    if (!hasRange(min, max)) {
+      return '-';
+    } else {
+      return `[${min ?? ''},${max ?? ''}]`;
+    }
+  };
+
+  const chunks = [''];
+
+  if (hasRange(minFav, maxFav)) {
+    chunks.push(`Fav: ${range(minFav, maxFav)}`);
+  }
+
+  if (hasRange(minRetweet, maxRetweet)) {
+    chunks.push(`RT: ${range(minRetweet, maxRetweet)}`);
+  }
+
+  if (hasRange(minReply, maxReply)) {
+    chunks.push(`Rp: ${range(minReply, maxReply)}`);
+  }
+
+  if (hasRange(minHashtag, maxHashtag)) {
+    chunks.push(`#: ${range(minHashtag, maxHashtag)}`);
+  }
+
+  if (sumTarget.length > 0) {
+    const map: Record<'fav' | 'retweet' | 'reply' | 'hashtag', string> = {
+      fav: 'F',
+      retweet: 'RT',
+      reply: 'Rp',
+      hashtag: '#',
+    };
+    chunks.push(
+      `${sumTarget.map((t) => map[t]).join('+')}: ${range(minSum, maxSum)}`
+    );
+  }
+  if (chunks.length === 1) {
+    chunks.push('N/A');
+  }
+  return chunks.join(' ');
+};
+
 export const rangeTextSelector = selector({
   key: 'rangeText',
   get: ({ get }) => {
-    const hasRange = (min: number | null, max: number | null) => {
-      return !(min === null && max === null);
-    };
-    const range = (min: number | null, max: number | null) => {
-      if (!hasRange(min, max)) {
-        return '-';
-      } else {
-        return `[${min ?? ''},${max ?? ''}]`;
-      }
-    };
-
-    const chunks = [''];
-
-    const { minFav, maxFav } = get(favRangeSelectorAtom);
-    if (hasRange(minFav, maxFav)) {
-      chunks.push(`Fav: ${range(minFav, maxFav)}`);
-    }
-
-    const { minRetweet, maxRetweet } = get(retweetRangeSelectorAtom);
-    if (hasRange(minRetweet, maxRetweet)) {
-      chunks.push(`RT: ${range(minRetweet, maxRetweet)}`);
-    }
-
-    const { minReply, maxReply } = get(replyRangeSelectorAtom);
-    if (hasRange(minReply, maxReply)) {
-      chunks.push(`Rp: ${range(minReply, maxReply)}`);
-    }
-
-    const { minHashtag, maxHashtag } = get(hashtagRangeSelectorAtom);
-    if (hasRange(minHashtag, maxHashtag)) {
-      chunks.push(`#: ${range(minHashtag, maxHashtag)}`);
-    }
-
-    const { minSum, maxSum } = get(sumRangeSelectorAtom);
-    const { sumTarget } = get(sumTypeSelectorAtom);
-    if (sumTarget.length > 0) {
-      const map: Record<'fav' | 'retweet' | 'reply' | 'hashtag', string> = {
-        fav: 'F',
-        retweet: 'RT',
-        reply: 'Rp',
-        hashtag: '#',
-      };
-      chunks.push(
-        `${sumTarget.map((t) => map[t]).join('+')}: ${range(minSum, maxSum)}`
-      );
-    }
-    if (chunks.length === 1) {
-      chunks.push('N/A');
-    }
-    return chunks.join(' ');
+    return getRangeText({
+      ...get(favRangeSelectorAtom),
+      ...get(retweetRangeSelectorAtom),
+      ...get(replyRangeSelectorAtom),
+      ...get(hashtagRangeSelectorAtom),
+      ...get(sumRangeSelectorAtom),
+      ...get(sumTypeSelectorAtom),
+    });
   },
 });
+
+export const getSourceText = ({
+  collectHashtags,
+  collectTweets,
+}: SourcePartialProps) =>
+  `${collectTweets.length} tweets / ${collectHashtags.length} tags`;
 
 export const sourceTextSelector = selector({
   key: 'sourceText',
   get: ({ get }) => {
-    const { collectHashtags, collectTweets } = get(sourceSelectorAtom);
-
-    return `${collectTweets.length} tweets / ${collectHashtags.length} tags`;
+    return getSourceText(get(sourceSelectorAtom));
   },
 });
 
+export const getResultText = ({
+  priority,
+  targetPattern,
+  timeout,
+}: ResultPartialProps) => {
+  if (timeout ?? 0 > 0) {
+    return `P: ${targetPattern} Pri: ${priority}, T: ${timeout} sec`;
+  } else {
+    return `P: ${targetPattern} Pri: ${priority}`;
+  }
+};
 export const resultTextSelector = selector({
   key: 'resultText',
   get: ({ get }) => {
-    const { priority, targetPattern } = get(resultAtom);
-
-    return `To ${targetPattern} with ${priority} priority`;
+    return getResultText(get(resultAtom));
   },
 });
