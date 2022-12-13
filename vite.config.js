f8fd16371df7ecdb308f60c15ffc0c4401e2af7d
f0reachARR <f0reach@f0reach.me>
2022-12-13 23:30:34 +0900

    Fix twitter embed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import vitePluginRequire from 'vite-plugin-require';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginRequire()],
   server: {
     proxy: {
       '/api': {
